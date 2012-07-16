efd52d984dc03a675d21e1f32e74f0657c5ee9bc
JeanMichel FRANCOIS <toutpt@gmail.com>
2012-07-16 17:02:24 +0200

    move to rst

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0'
 setup(name='captionstransformer',
       version=version,
       description="",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
