d5ed829f120b95d29362805ae38b945350336469
JeanMichel FRANCOIS <toutpt@gmail.com>
2012-07-26 17:04:13 +0200

    add extension and mimetype info into REGISTRY

--- captionstransformer/registry.py
+++ captionstransformer/registry.py
@@ -1,10 +1,18 @@
 from captionstransformer import transcript, ttml, sbv, srt
 
 REGISTRY = {'transcript':{'reader': transcript.Reader,
-                    'writer': transcript.Writer},
+                          'writer': transcript.Writer,
+                          'mimetype': 'text/xml',
+                          'extension': '.xml'},
             'TTML':{'reader': ttml.Reader,
-                    'writer': ttml.Writer},
+                    'writer': ttml.Writer,
+                    'mimetype': 'application/ttml+xml',
+                    'extension': '.xml'},
             'SBV': {'reader': sbv.Reader,
-                    'writer': sbv.Writer},
+                    'writer': sbv.Writer,
+                    'mimetype': 'text/plain',
+                    'extension': '.sbv'},
             'SRT': {'reader': srt.Reader,
-                    'writer': srt.Writer}}
+                    'writer': srt.Writer,
+                    'mimetype': 'text/plain',
+                    'extension': '.srt'}}
