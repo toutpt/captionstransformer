6e33cabd55b6bdb59d2a776b2b798d3cd355ceb4
Brennan Young <brennan@young.net>
2017-02-09 16:26:10 +0100

    support for sub-second (i.e. milliseconds) timecode values
    Specifications for ttml time attributes permit sub-second values
    (milliseconds, frames, ticks...), but captionstransformer fails to parse
    these.
    
    ttml.py is only looking for '%H:%M:%S' causing the exception "ValueError
    unconverted data remains". The %f macro was added to strftime/strptime
    in python 2.6 to handle these units.
    
    This commit adds support for milliseconds. It would be fairly
    straightforward to add support for other sub-second values.

--- captionstransformer/ttml.py
+++ captionstransformer/ttml.py
@@ -16,15 +16,23 @@ class Reader(core.Reader):
         return self.captions
 
     def get_date(self, time_str):
-        return datetime.strptime(time_str, '%H:%M:%S')
-
+        try:
+            convertedTime = datetime.strptime(end_date, '%H:%M:%S')
+        except ValueError as v:
+            ulr = len(v.args[0].partition('unconverted data remains: ')[2])
+            if ulr:
+                convertedTime = datetime.strptime(end_date, "%H:%M:%S.%f")
+            else:
+                raise v
+        return convertedTime
+        
 class Writer(core.Writer):
     DOCUMENT_TPL = u"""<tt xml:lang="" xmlns="http://www.w3.org/ns/ttml"><body><div>%s</div></body></tt>"""
     CAPTION_TPL = u"""<p begin="%(start)s" end="%(end)s">%(text)s</p>"""
 
     def format_time(self, caption):
         """Return start and end time for the given format"""
-        #should be seconds by default
-
-        return {'start': caption.start.strftime('%H:%M:%S'),
-                'end': caption.end.strftime('%H:%M:%S')}
+        
+        # Milliseconds given because of the [:-3]
+        return {'start': caption.start.strftime('%H:%M:%S.%f')[:-3],
+                'end': caption.end.strftime('%H:%M:%S.%f')[:-3]}
